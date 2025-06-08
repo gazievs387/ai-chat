0e0163bcc5c4c72e3e6496251b7f9d7312cbe612
Said <gazievs387@gmail.com>
2025-06-08 17:55:09 +0300

    react, webpack, typescript base setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./index.css"
+import GPT4Image from "./static/gpt4.png"
+
+
+function App() {
+    return (
+        <main>
+            <h1>11111111 One</h1>
+
+            <img src={GPT4Image} />
+        </main>
+    )
+}
+
+
+export default App;
---
+++ src/types/global.d.ts
@@ -0,0 +1,7 @@
+
+
+
+declare module "*.png" {
+    const content: string;
+    export default content
+}
