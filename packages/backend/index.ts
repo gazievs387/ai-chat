b9ed74dfaf661279539fbb1a7926536874b11df6
Said <gazievs387@gmail.com>
2025-09-25 19:59:37 +0300

    setup build npm script on backend

---
+++ packages/backend/index.ts
@@ -0,0 +1,23 @@
+import express from "express"
+import cors from "cors"
+import { router } from "./src/routes/routes";
+
+
+const app = express()
+const port = 3001
+
+app.use(cors())
+
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send()
+})
+
+app.use(router)
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
